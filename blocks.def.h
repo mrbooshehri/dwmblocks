1363812910035bd207a7d36ad512b9f3cebd4a43
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-01-01 17:33:17 +0300

    backlight, battery, spotify and keyboard blocks added

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%b %d %H:%M'",					60,		0},
+	{"",	 "~/scripts/dwmblocks/spotify.sh",			30,		0},
+	{"  ",	 "~/scripts/dwmblocks/keyboard.sh",			0,		0},
+	{"", "~/scripts/dwmblocks/battery.sh",				300,		0},
+	{"", "~/scripts/dwmblocks/backlight.sh",			0,		11},
 	{"墳  ", "~/scripts/dwmblocks/volume.sh",			0,		10},
+	{"", "date '+%b %d %H:%M'",					60,		0},
 /*	{"", "~/scripts/dwmblocks/bluetooth.sh",			0,		0},*/
 };
 
