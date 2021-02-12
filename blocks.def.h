7907e87fa4b7072acceddbc49023902a4d655dbb
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-02-12 11:50:12 +0300

    checkupdare added

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	 "~/scripts/dwmblocks/spotify.sh",			30,		0},
+/*	{"",	 "~/scripts/dwmblocks/spotify.sh",			30,		0},*/
+	{"  ",	 "~/scripts/dwmblocks/checkupdate.sh",			3600,		0},
 	{"  ",	 "~/scripts/dwmblocks/keyboard.sh",			0,		0},
 	{"", "~/scripts/dwmblocks/battery.sh",				300,		0},
 	{"", "~/scripts/dwmblocks/backlight.sh",			0,		11},
