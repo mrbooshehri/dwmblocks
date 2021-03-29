849e51b853222f659357efdd3abc55feaf97dd68
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-03-29 20:47:10 +0300

    minor edits

--- blocks.def.h
+++ blocks.def.h
@@ -4,11 +4,12 @@ static const Block blocks[] = {
 	{" ",	 "~/scripts/dwmblocks/checkupdate.sh",			3600,		0},
 	{"", "~/scripts/dwmblocks/battery.sh",				300,		0},
 	{"", "~/scripts/dwmblocks/backlight.sh",			0,		11},
-	{" ", "~/scripts/dwmblocks/volume.sh",			0,		10},
-	{"  ", "date '+%b %d'",						60,		0},
+	{" ", "~/scripts/dwmblocks/volume.sh",				0,		10},
+	{"  ", "date '+%b %d'",					60,		0},
 	{" ", "date '+%H:%M'",						60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
+// 
 static unsigned int delimLen = 5;
