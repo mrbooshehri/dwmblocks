3d27f4c75ee95047c3940f494bb2fb2a1ed5194c
mohammadreza booshehri <mrbooshehri@gmail.com>
2022-02-10 12:34:04 +0300

    correct path add logo for brightness

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ",	 "~/Repos/github.com/mrbooshehri/dot/scripts/checkupdate",			3600,		0},
-	{"", "~/Repos/github.com/mrbooshehri/dot/scripts/battery",				300,		0},
-	{"", "~/Repos/github.com/mrbooshehri/dot/scripts/backlight",			0,		11},
-	{"", "~/Repos/github.com/mrbooshehri/dot/scripts/volume",				0,		10},
+	{" ",	 "$HOME/Repos/github.com/mrbooshehri/dot/scripts/checkupdate",			3600,		0},
+	{"", "$HOME/Repos/github.com/mrbooshehri/dot/scripts/battery",				300,		0},
+	{"盛  ", "$HOME/Repos/github.com/mrbooshehri/dot/scripts/backlight",			0,		11},
+	{"", "$HOME/Repos/github.com/mrbooshehri/dot/scripts/volume",				0,		10},
 	{"  ", "date '+%b %d'",					60,		0},
 	{" ", "date '+%H:%M'",						60,		0},
 };
